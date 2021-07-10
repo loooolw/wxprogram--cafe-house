e1a0193eab64887da4ae0a38e0fa5cb256b70b92
大罗皮蛋 <86958918+loooolw@users.noreply.github.com>
2021-07-10 18:01:58 +0800

    Add files via upload

---
+++ pages/kefu/kefu.js
@@ -0,0 +1,17 @@
+// pages/kefu/kefu.js
+Page({
+
+  data: {
+markers:[
+  {
+    id:1,
+    longitude:"113.37779599759094",
+    latitude:"23.13211891250585",
+    title:"广州叩丁狼",
+    width:20,
+    height:20
+  }]
+  },
+
+  
+})
